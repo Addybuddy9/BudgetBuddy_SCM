5a16bb333a5fec28abb00dd3c009178d08c59f15
Pratham Gupta <prathamgupta00715@gmail.com>
2025-05-02 13:48:20 +0530

    Updated Product.tsx

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -27,7 +27,7 @@ const ProductDetails = () => {
         <div className="flex items-center justify-center h-64">
           <div className="text-center">
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-600 mx-auto"></div>
-            <p className="mt-4 text-gray-600">Loading product details...</p>
+            <p className="mt-4 text-gray-600">Loading product Details...</p>
           </div>
         </div>
       </div>
