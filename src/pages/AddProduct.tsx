9822ccf1f0bec28e15e5cab4fa9fbf9d76c942d2
Addybuddy9 <addybuddy9@gmail.com>
2025-05-05 22:39:25 +0530

    Update AddProduct.tsx
    removing erroes

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -132,23 +132,6 @@ const AddProduct = () => {
                     className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                   >
                     <span>Upload a file</span>
-                    <input
-                      id="file-upload"
-                      name="file-upload"
-                      type="file"
-                      className="sr-only"
-                      accept="image/*"
-                      onChange={handleImageChange}
-                    />
-                  </label>
-                  <p className="pl-1">or drag and drop</p>
-                </div>
-                <p className="text-xs text-gray-500">
-                  PNG, JPG, GIF up to 10MB
-                </p>
-              </div>
-            </div>
-          </div>
 
           <button
             type="submit"
